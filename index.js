f11397ed4ed94b490f6261008efe1cb282d10dc9
Jarugulla Ganapthi <115489205+Ganipj@users.noreply.github.com>
2023-02-03 15:38:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const hex = document.querySelector(".Color");
+const button = document.querySelector(".generate");
+
+const generateColor = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8);
+    document.body.style.backgroundColor = "#" + randomColor;
+    hex.innerHTML = "#" + randomColor;
+}
+button.addEventListener("click", generateColor);
+
